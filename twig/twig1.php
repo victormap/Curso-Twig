64c8c016399053836a424bf4e04cde1b19dbfa9f
Víctor Manuel <victormanuelap@gmail.com>
2017-03-16 14:44:13 +0100

    Se crea un comentario específico para luego conectar el cambio con GitHub

--- twig/twig1.php
+++ twig/twig1.php
@@ -1,5 +1,6 @@
 <?php
-    require_once '/usr/share/php/Twig/Autoloader.php';
+    // Comentario creado para luego subir el archivo a GitHub
+	require_once '/usr/share/php/Twig/Autoloader.php';
     Twig_Autoloader::register();
 
     $loader = new Twig_Loader_String();
